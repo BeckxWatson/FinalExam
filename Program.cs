e2269a4126ee511e6692b37154379897530bde1b
Kittichai <kittichai.kitt@mail.kmutt.ac.th>
2023-05-15 15:34:05 +0700

    Final Exam

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program {
+    static void Main ()
+    {
+        Console.Write("numberOfcities: ");
+        int numberOfCities = int.Parse(Console.ReadLine());
+
+        City[] cities = new City[numberOfCities];
+
+        for (int i = 0; i < numberOfCities; i++)
+        {
+            Console.WriteLine("CityNumber {i}");
+            Console.Write("Cityname: ");
+            string Cityname = Console.ReadLine();
+
+            Console.Write("PopuInCity: ");
+            int population = int.Parse(Console.ReadLine());
+
+            cities[i] = new City(i, Cityname, population);
+        }
+
+        Console.WriteLine("AllInfoOfCity: ");
+        foreach (City city in cities)
+        {
+            Console.WriteLine(city.GetCityInfo());
+        }
+    }
+}
+class City
+{
+    private int cityID;
+    private string cityName;
+    private int population;
+    
+    public City(int id, string name, int pop)
+    {
+        cityID = id;
+        cityName = name;
+        population = pop;
+    }
+    public string GetCityInfo()
+    {
+        return "CityNumber {cityID}: {cityName}, PopuInCity {population}:";
+    }
+}
